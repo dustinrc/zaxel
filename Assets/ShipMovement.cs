46f03a0e0352776a2fdf1c78e141e5bf7a72ff52
Dustin Chapman <dustin.r.chapman@gmail.com>
2015-09-16 00:19:44 -0400

    start of ship movement

---
+++ Assets/ShipMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMovement : MonoBehaviour
+{
+	public float hSpeed = 15.0f;
+	public float vSpeed = 15.0f;
+	public float hBoundaryMin = -15.0f;
+	public float hBoundaryMax = 15.0f;
+	public float vBoundaryMin = 0.0f;
+	public float vBoundaryMax = 25.0f;
+
+	void Update ()
+	{
+		float hTranslation = Input.GetAxis ("Horizontal") * hSpeed * Time.deltaTime;
+		float vTranslation = Input.GetAxis ("Vertical") * vSpeed * Time.deltaTime;
+		Vector3 pos = transform.position;
+		pos.x = Mathf.Clamp (pos.x + hTranslation, hBoundaryMin, hBoundaryMax);
+		pos.z = Mathf.Clamp (pos.z + vTranslation, vBoundaryMin, vBoundaryMax);
+		transform.position = pos;
+	}
+}
