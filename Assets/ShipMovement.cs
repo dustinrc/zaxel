2ff4e45edd6edeeb6ff64e2cdf732caa540ccc17
Dustin Chapman <dustin.r.chapman@gmail.com>
2015-09-20 11:46:01 -0400

    acceleration and deceleration of ship

--- Assets/ShipMovement.cs
+++ Assets/ShipMovement.cs
@@ -3,17 +3,43 @@ using System.Collections;
 
 public class ShipMovement : MonoBehaviour
 {
-	public float hSpeed = 15.0f;
-	public float vSpeed = 15.0f;
+	public float hMaxSpeed = 15.0f;
+	public float vMaxSpeed = 15.0f;
+	public float hAcceleration = 45.0f;
+	public float hDeceleration = 4.5f;
+	public float vAcceleration = 45.0f;
+	public float vDeceleration = 4.5f;
 	public float hBoundaryMin = -15.0f;
 	public float hBoundaryMax = 15.0f;
 	public float vBoundaryMin = 0.0f;
 	public float vBoundaryMax = 25.0f;
+	private float hSpeed = 0.0f;
+	private float vSpeed = 0.0f;
 
 	void Update ()
 	{
-		float hTranslation = Input.GetAxis ("Horizontal") * hSpeed * Time.deltaTime;
-		float vTranslation = Input.GetAxis ("Vertical") * vSpeed * Time.deltaTime;
+		float hAxis = Input.GetAxis ("Horizontal");
+		float vAxis = Input.GetAxis ("Vertical");
+		float dt = Time.deltaTime;
+		if (hAxis != 0.0f) {
+			hSpeed = hSpeed + hAxis * hAcceleration * dt;
+		} else {
+			hSpeed = Mathf.LerpUnclamped (hSpeed, 0.0f, hDeceleration * dt);
+			if (Mathf.Abs (hSpeed) <= 0.01f)
+				hSpeed = 0.0f;
+		}
+		hSpeed = Mathf.Clamp (hSpeed, -hMaxSpeed, hMaxSpeed);
+		if (vAxis != 0.0f) {
+			vSpeed = vSpeed + vAxis * vAcceleration * dt;
+		} else {
+			vSpeed = Mathf.LerpUnclamped (vSpeed, 0.0f, vDeceleration * dt);
+			if (Mathf.Abs (vSpeed) <= 0.01f)
+				vSpeed = 0.0f;
+		}
+		vSpeed = Mathf.Clamp (vSpeed, -vMaxSpeed, vMaxSpeed);
+		print ("hSpeed: " + hSpeed + ", vSpeed: " + vSpeed);
+		float hTranslation = hSpeed * dt;
+		float vTranslation = vSpeed * dt;
 		Vector3 pos = transform.position;
 		pos.x = Mathf.Clamp (pos.x + hTranslation, hBoundaryMin, hBoundaryMax);
 		pos.z = Mathf.Clamp (pos.z + vTranslation, vBoundaryMin, vBoundaryMax);
