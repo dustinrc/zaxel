2ad4e93e0b2461c0d262c6778859e85bb46cfb25
Dustin Chapman <dustin.r.chapman@gmail.com>
2015-10-04 22:52:41 -0400

    kilo enemy movement

---
+++ Assets/EnemyKiloMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyKiloMovement : MonoBehaviour
+{
+
+	public float maxSpeed = 1.0f;
+	private Transform target;
+	private Rigidbody rb;
+
+	void Awake ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		rb.useGravity = false;
+	}
+
+	void Start ()
+	{
+		target = GameObject.FindWithTag ("Player").transform;
+	}
+	
+	void Update ()
+	{
+		transform.LookAt (target);
+		rb.AddForce (transform.forward * 0.3f, ForceMode.Impulse);
+
+		if (rb.velocity.magnitude > maxSpeed)
+			rb.velocity = rb.velocity.normalized * maxSpeed;
+	}
+}
